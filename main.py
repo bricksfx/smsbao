8f729c3833f541181bd729bd39c45f0ba8aabf59
acm <acm@acm.sut.edu.cn>
2015-04-19 21:16:11 +0800

    Committer: acm <acm@acm.sut.edu.cn> 	modified:   main.py 	deleted:    test.py 	deleted:    test1.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dbconn import conndb
 def send_mes_to_team():
     team_data = conndb()
     for team_single in team_data:
-        content = unicode("from acmlib") + u' ' + team_single['user_id'] +unicode(" 的队长") + unicode("我们将于明天开始热身赛请做好准备")
+        content = unicode("from acmlab") + u' ' + team_single['user_id'] +unicode(" 的队长") + unicode("我们将于明天开始热身赛请做好准备")
         tel = team_single['team_telephone']
         print content
         sendstatus = mesSend(tel, content)
