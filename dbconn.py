aeaa79c212c8d84ff90a5807bc1ceb2e9505f24c
brickfx <252142844@qq.com>
2015-04-19 20:07:39 +0800

    修改:         .idea/workspace.xml 	修改:         dbconn.py

--- dbconn.py
+++ dbconn.py
@@ -2,4 +2,10 @@
 import MySQLdb
 
 def conndb():
-    conn = MySQLdb.conn
+    conn=MySQLdb.connect(host="localhost",user="root",passwd="000000",db="jol",charset="utf8")
+    cur = conn.cursor()
+    cur.execute("select * from users")
+    for data in cur.fetchall():
+        print data
+    cur.close()
+    
